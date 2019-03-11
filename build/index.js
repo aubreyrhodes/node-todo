682fcf52c4a27cdb4b8b87085dbaff6521b3456c
Aubrey Rhodes <aubrey.c.rhodes@gmail.com>
2019-03-10 22:13:53 -0400

    Remove build output from git

--- build/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";var _server = _interopRequireDefault(require("./server"));function _interopRequireDefault(obj) {return obj && obj.__esModule ? obj : { default: obj };}
-
-var port = 3000;
-
-_server.default.listen(port);
-//# sourceMappingURL=index.js.map
