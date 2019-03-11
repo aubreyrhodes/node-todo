4a184b138745144b0eed1ac59c14f88870afdd44
Aubrey Rhodes <aubrey.c.rhodes@gmail.com>
2019-03-10 22:49:27 -0400

    fix static path

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import Express from 'express';
 import ExpressHandlebars from 'express-handlebars';
+import path from 'path';
 
 const app = Express();
 const hbs = ExpressHandlebars.create({ defaultLayout: 'main' });
@@ -7,7 +8,7 @@ const hbs = ExpressHandlebars.create({ defaultLayout: 'main' });
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
-app.use(Express.static('public'));
+app.use(Express.static(path.join(__dirname, '../public')));
 
 app.get('/', (_, res) => {
     res.render('home');
